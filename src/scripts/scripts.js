965605fc44c66f924c9d29e5c79a89b998019a74
Anton Shchukin <a.a.shchukin@gmail.com>
2024-04-14 12:58:08 +0700

    catalog

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,17 +1,20 @@
 (function ($) {
 
-    const catalogSlider = new Swiper('.swiper--catalog', {
-        navigation: {
-            prevEl: '.swiper__control--prev',
-            nextEl: '.swiper__control--next',
-            disabledClass: 'swiper__control--disabled',
-        },
-        mousewheel: {
-            forceToAxis: true,
-        },
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 4,
-    });
+    if( window.matchMedia('(min-width: 600px)').matches ) { /* На десктопах и планшетах */
+
+        const catalogSlider = new Swiper('.swiper--catalog', {
+            navigation: {
+                prevEl: '.swiper__control--prev',
+                nextEl: '.swiper__control--next',
+                disabledClass: 'swiper__control--disabled',
+            },
+            mousewheel: {
+                forceToAxis: true,
+            },
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 4,
+        });
+    }
 
 })(jQuery);
